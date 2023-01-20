f79ad934f306e7e3a5549dc2c55ee93b6bc0b9a0
Khavharendwe <kdnemangaya@gmail.com>
2023-01-20 15:56:33 +0200

    added service

--- frontend/studentManager/src/app/components/login/login.component.ts
+++ frontend/studentManager/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  form!: FormGroup;
+  loading = false;
+  submitted = false;
 
+  user: any;
+  password:string = "";
+  email: string ="";
+  usertype: number = 0;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService
+) { }
+
+ngOnInit(): void {
+  this.login();
+
+}
+
+
+  onSubmit(): void{
+    //this.userService.getUser().subscribe(data =>{
+      //alert(data.toString());
+      //console.log(data)
+    //})
+  }
+
+  login(): void{
+    var val = {
+      
+      email:this.email,
+      password:this.password,
+      usertype: this.usertype
+    };
+
+    this.userService.createUser(val).subscribe(res =>{
+      console.log(val)
+    })
+
+
+    switch (this.usertype) {
+
+      case 1:
+          console.log("It is a Monday.");
+          break;
+      case 2:
+          console.log("It is a Tuesday.");
+          break;    
+      default: // redirect to home
+          console.log("No such day exists!");
+          break;
+    
+        }
+  }
 }
