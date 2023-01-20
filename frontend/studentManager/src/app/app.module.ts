8c19b19e7d86737d518b3e925b03e91298f8ec8b
Khavharendwe <kdnemangaya@gmail.com>
2023-01-20 15:19:25 +0200

    added components

--- frontend/studentManager/src/app/app.module.ts
+++ frontend/studentManager/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentDashboardComponent } from './components/student-dashboard/student-dashboard.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentDashboardComponent,
+    AdminDashboardComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
